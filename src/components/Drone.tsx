077509d9b7c7bc63e0ebbe412c5059e648c99e91
shaggy611 <sevczik11@gmail.com>
2023-11-27 01:51:15 +0200

    Fix Drone moving inside the Cave

--- src/components/Drone.tsx
+++ src/components/Drone.tsx
@@ -1,40 +1,39 @@
-import { KeyboardEvent, useEffect, useRef } from 'react'
+import { KeyboardEvent, useEffect } from 'react'
 import styled from 'styled-components'
 import { useBoundStore } from '../store'
 
 function Drone() {
   const position = useBoundStore((state) => state.position)
   const setPosition = useBoundStore((state) => state.setPosition)
-  const dronRef = useRef(null)
+
+  const dronePosition = {
+    transform: `translate(${position[0]}px, ${position[1]}px)`,
+  }
+
+  function changeDronePosition(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'ArrowLeft':
+        setPosition([(position[0] -= 1), position[1]])
+        break
+      case 'ArrowRight':
+        setPosition([(position[0] += 1), position[1]])
+        break
+      case 'ArrowUp':
+        setPosition([position[0], (position[1] -= 1)])
+        break
+      case 'ArrowDown':
+        setPosition([position[0], (position[1] += 1)])
+        break
+    }
+  }
 
   useEffect(() => {
-    const dron = dronRef.current
-    function changeDronePosition(event: KeyboardEvent) {
-      console.log('test')
-      switch (event.key) {
-        case 'ArrowLeft':
-          setPosition([position[0] - 1, position[1]])
-          break
-        case 'ArrowRight':
-          setPosition([position[0] + 1, position[1]])
-          break
-        case 'ArrowUp':
-          setPosition([position[0], position[1] - 1])
-          break
-        case 'ArrowDown':
-          setPosition([position[0], position[1] + 1])
-          break
-      }
-    }
+    document.addEventListener('keydown', changeDronePosition)
 
-    if (dron) {
-      dron.addEventListener('keydown', changeDronePosition)
-    }
-
-    return () => dron.removeEventListener('keydown', changeDronePosition)
+    return () => document.removeEventListener('keydown', changeDronePosition)
   }, [])
 
-  return <StyledDrone dronePosition={position} tabIndex={0} ref={dronRef} />
+  return <StyledDrone dronePosition={position} style={dronePosition} />
 }
 
 export default Drone
@@ -42,8 +41,6 @@ export default Drone
 const StyledDrone = styled.div<{
   dronePosition: number[]
 }>`
-  transform: ${(props) =>
-    `translate(${props.dronePosition[0]}px, ${props.dronePosition[1]}px)`};
   width: 30px;
   height: 30px;
   background-color: red;
