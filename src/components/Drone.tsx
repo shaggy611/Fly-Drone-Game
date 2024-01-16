b4fcc30748ab35d7ed788214c52cfcab9c5150fb
shaggy611 <sevczik11@gmail.com>
2024-01-17 01:23:56 +0200

    Fix useDroneSpeed hook + fix physics of drone move

--- src/components/Drone.tsx
+++ src/components/Drone.tsx
@@ -54,7 +54,7 @@ export default function Drone() {
         setStart()
       }
     }
-  }, [horizontalSpeed, caveBlockHeight, setGameFailed, setStart])
+  }, [caveBlockHeight, setGameFailed, setStart])
 
   return (
     <>
