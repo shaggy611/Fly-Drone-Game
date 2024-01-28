2ee6babbe85b4d47c38b8c8e3e12b8ab747435a9
shaggy611 <sevczik11@gmail.com>
2024-01-28 18:14:01 +0200

    Removed commented code from Cave component

--- src/components/Cave.tsx
+++ src/components/Cave.tsx
@@ -1,19 +1,12 @@
 import { useEffect, useRef } from 'react'
 import { useBoundStore } from '../store'
 import styled from 'styled-components'
-// import apiGetCaveCoords from '../utils/api/apiGetCaveCoords'
-// import prepareCoordsForSVG from '../utils/prepareCoordsForSVG'
-// import { apiInitGame } from '../utils/api/apiGameInitialize'
 import CaveSVG from './CaveSVG'
 import useGameInit from '../hooks/useGameInit'
 
 function Cave() {
-  // const cords = useBoundStore((state) => state.caveCoords)
-  // const token = useBoundStore((state) => state.token)
   const loading = useBoundStore((state) => state.loading)
-  // const setLoading = useBoundStore((state) => state.setLoading)
   const caveSvgPoints = useBoundStore((state) => state.caveSvgPoints)
-  // const setCaveSvgPoints = useBoundStore((state) => state.setCaveSvgPoints)
   const verticalSpeed = useBoundStore((state) => state.verticalSpeed)
   const verticalTraveledDistance = useBoundStore(
     (state) => state.verticalTraveledDistance
@@ -23,7 +16,6 @@ function Cave() {
   )
   const caveRef = useRef<HTMLDivElement | null>(null)
   const speedIntervalRef = useRef<number | null>(null)
-  // const setGameDataLoaded = useBoundStore((state) => state.setGameDataLoaded)
 
   useEffect(() => {
     function droneVerticalSpeedChange() {
@@ -46,33 +38,6 @@ function Cave() {
   }, [verticalTraveledDistance])
 
   useGameInit()
-
-  // useEffect(() => {
-  //   async function initGame() {
-  //     await apiInitGame()
-  //   }
-  //   initGame()
-  //   setLoading()
-  // }, [setLoading])
-
-  // useEffect(() => {
-  //   async function getCaveCoords() {
-  //     await apiGetCaveCoords()
-  //   }
-
-  //   if (token.length > 0) {
-  //     getCaveCoords()
-  //   }
-  // }, [token])
-
-  // useEffect(() => {
-  //   if (cords.length > 0) {
-  //     const stringSVG = prepareCoordsForSVG(cords)
-  //     setCaveSvgPoints(stringSVG)
-  //     setLoading()
-  //     setGameDataLoaded()
-  //   }
-  // }, [cords, setCaveSvgPoints, setLoading])
 
   return (
     <>
