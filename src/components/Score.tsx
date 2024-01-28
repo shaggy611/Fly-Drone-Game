f783924f8441f16d8da6e23e8cc7f8f5d73d2fa4
shaggy611 <sevczik11@gmail.com>
2024-01-28 18:19:06 +0200

    Added appearing animation for Score Component

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,5 +1,5 @@
 import { Container, Typography } from '@mui/material'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { useBoundStore } from '../store'
 import useScore from '../hooks/useScore'
 
@@ -27,6 +27,16 @@ function Score() {
 
 export default Score
 
+const appearing = keyframes`
+    10% { transform:translateX(0%); opacity: 0; }
+    30% { transform:translateX(-25%); }
+    50% { transform:translateX(0%); }
+    60% { transform:translateX(-7%); }
+    80% { transform:translateX(0%); }
+    98% { transform:translateX(-3%); }
+    100% { transform:translateX(0); opacity: 1; }
+`
+
 const StyledScore = styled(Container)`
   position: absolute;
   left: 10px;
@@ -35,4 +45,5 @@ const StyledScore = styled(Container)`
   max-width: 280px !important;
   padding: 8px;
   border-radius: 10px;
+  animation: ${appearing} 0.5s ease-in 0.2s;
 `
